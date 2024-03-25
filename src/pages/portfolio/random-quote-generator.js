53edb8a32c9b3d6b24d72e2fdaa051b3c7bad81c
Moiz Hasan <moiz_hasan@yahoo.com>
2024-03-25 16:37:30 -0500

    updated quote generator to use hooks

--- src/pages/portfolio/random-quote-generator.js
+++ src/pages/portfolio/random-quote-generator.js
@@ -1,96 +1,74 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Layout from '../../components/layout.js'
 import * as styles from './random-quote-generator.module.css'
 import { FaTwitter } from 'react-icons/fa'
 /*
  * Random Quote Generation with React
  */
-class QuoteBox extends React.Component {
-  constructor(props) {
-        super(props);
-        
-    this.state = {
-        quotes: [],
-        quote: '',
-        author: '',
-        backgroundColor: ''
-        };
-    this.setQuote = this.setQuote.bind(this);
-  }
-  
-  updateBackground() {
+const QuoteBox = () => {
+
+    const [quotes, setQuotes] = useState();
+    const [quote, setQuote] = useState();
+    const [author, setAuthor] = useState();
+    const [backgroundColor, setBackGroundColor] = useState();
+
+  const updateBackground = () => {
     // get random number.
-    const colors = [0,0,0].map(x => x + Math.floor(Math.random() * 200)); 
-    let color = 'rgb(' + colors[0] + ',' + colors[1]+ ',' + colors[2] + ')';
-    this.setState (
-      {
-        backgroundColor: color,
-      }
-    );
+    const colors = [0, 0, 0].map(x => x + Math.floor(Math.random() * 200));
+    let color = 'rgb(' + colors[0] + ',' + colors[1] + ',' + colors[2] + ')';
+    setBackGroundColor(color);
   }
-  
-  loadQuotes() {
+
+  const genQuote = (quotes) => {
+    let x = Math.floor(Math.random() * (quotes.length - 1));
+    setQuote(quotes[x].quote.replace(/<(?:.|\n)*?>/gm, ''));
+    setAuthor(quotes[x].author);
+    updateBackground();
+  };
+
+  const loadQuotes = () => {
     const quotes = [];
-   fetch("https://en.wikiquote.org/w/api.php?    &origin=*&action=parse&page=Wikiquote:Quote%20of%20the%20Day&format=json", 
-    {
-      method: 'GET',
-    }
-     )
+    fetch("https://en.wikiquote.org/w/api.php?    &origin=*&action=parse&page=Wikiquote:Quote%20of%20the%20Day&format=json",
+      {
+        method: 'GET',
+      }
+    )
       .then(response => response.json())
-      .then(data => { //use arrow function to preserve 'this'
+      .then(data => {
         var text = data["parse"]["text"];
         var list = JSON.stringify(text).split('<li>');
         var author = '';
-        var quote =  '';
-        for (let i = 1; i < list.length-1; i++) {
-          quote = list[i].substring(0, list[i].indexOf('~')-1);
-          author = list[i].substring(list[i].indexOf('">')+2, list[i].indexOf('</a'));
-          quotes.push({author: author, quote: quote});
+        var quote = '';
+        for (let i = 1; i < list.length - 1; i++) {
+          quote = list[i].substring(0, list[i].indexOf('~') - 1);
+          author = list[i].substring(list[i].indexOf('">') + 2, list[i].indexOf('</a'));
+          quotes.push({ author: author, quote: quote });
         }
-        this.setState({
-          quotes: quotes,
-        });
-        
-        setTimeout(() => {
-          console.log("Delayed for 1 second.");
-          this.setQuote();
-        }, 1000);
-    });
-    }
-  
-  setQuote() {
-    let x = Math.floor(Math.random() * (this.state.quotes.length - 1));
-		this.setState(
-            {
-              quote: this.state.quotes[x].quote.replace(/<(?:.|\n)*?>/gm, ''),
-              author: this.state.quotes[x].author
-            }
-        );
-    this.updateBackground();
-	};
-
-  componentDidMount() {
-    this.loadQuotes();
+          setQuotes(quotes);
+          genQuote(quotes);
+      });
   }
-  
-  render() {
+
+  useEffect(() => {
+    loadQuotes();
+  }, []);
+
     return (
-    <Layout>
-    <div id="quote-box" className={styles.quoteBox} style = {{backgroundColor: this.state.backgroundColor}}>
-      <h2 id="text" className={styles.text}>"{this.state.quote}"</h2>
-            <p id="author" className={styles.author}>-{this.state.author}</p>
-      <div>
-        <button id="new-quote" className={styles.newQuote} onClick={ this.setQuote }>
-				      New Quote
-			      </button>
-        <a style={{marginLeft:'20px', textDecoration:'none'}} id="tweet-quote" href={"https://twitter.com/intent/tweet?text=" 
-        + '"' + this.state.quote + '"  - ' + this.state.author} target="_blank" rel="noopener noreferrer">
-            <FaTwitter/>Share on Twitter</a>
-      </div>
-    </div>
-    </Layout>
+      <Layout>
+        <div id="quote-box" className={styles.quoteBox} style={{ backgroundColor: backgroundColor }}>
+          <h2 id="text" className={styles.text}>"{quote}"</h2>
+          <p id="author" className={styles.author}>-{author}</p>
+          <div>
+            <button id="new-quote" className={styles.newQuote} onClick={() => genQuote(quotes)}>
+              New Quote
+            </button>
+            <a style={{ marginLeft: '20px', textDecoration: 'none' }} id="tweet-quote" href={"https://twitter.com/intent/tweet?text="
+              + '"' + quote + '"  - ' + author} target="_blank" rel="noopener noreferrer">
+              <FaTwitter />Share on Twitter</a>
+          </div>
+        </div>
+      </Layout>
     )
   }
-}
 
 export default QuoteBox;
